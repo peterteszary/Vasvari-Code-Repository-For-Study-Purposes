d9c3acab67a32aa60969e677dc4f680c1f73f74f
Teszáry Péter <peterteszary@gmail.com>
2022-09-26 15:51:21 +0200

    Változók felcserélése

---
+++ Python Projects/ket-valtozo-ertekenek-felcserelese.py
@@ -0,0 +1,14 @@
+szam1 = input("Kérem az első számot:")
+szam2 = input("Kérem a második számot:")
+
+print("Az első szám:", szam1)
+print("A második szám:", szam2)
+
+temp = szam1
+szam1 = szam2
+szam2 = temp
+
+print("Felcsrélve a kettő = A első szám szám:", szam1, "A második szám:", szam2)
+
+
+# létrehozunk egy segédváltozót --> temp = szam 1
