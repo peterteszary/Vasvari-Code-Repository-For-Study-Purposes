8c62a191541bb63eac8a8f0d455f6d89bc427ab7
Teszáry Péter <peterteszary@gmail.com>
2022-09-26 16:02:30 +0200

    típusok

---
+++ Python Projects/tipusok.py
@@ -0,0 +1,15 @@
+szam1 = input("Kérek egy számot: ")
+szoveg =" HELLO "
+print(type(szam1))
+print(type(szoveg))
+
+szam1 = int(szam1)
+print(szam1)
+print(type(szam1))
+
+tort = 1.15
+print(type(tort))
+tort = int(tort)
+
+print(tort)
+print(type(tort))
