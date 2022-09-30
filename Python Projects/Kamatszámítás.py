f79de8d517abfcae055b2a57be3ae9ca79856a70
Teszáry Péter <peterteszary@gmail.com>
2022-09-30 18:23:25 +0200

    Órai feladatok

--- Python Projects/Kamatszámítás.py
+++ Python Projects/Kamatszámítás.py
@@ -1,1 +1,8 @@
 """Írjunk kamatszámító programot Összeg=alap*(1+kamat/100/12*hónap)"""
+
+alap = float(input("Kérem a befektetni való összeget:" ))
+kamat = float(input("Kérem az aktuális kamatot: "))
+honap = int(input("Kérem a hónapok számát -> a befektetés hosszát: "))
+
+osszeg = alap * (1 + kamat /100/12 * honap)
+print("A FUTAMIDŐ VÉGÉN VÁRHATÓ BEFEKTETÉSI ÉRTÉK: ", int(osszeg))
