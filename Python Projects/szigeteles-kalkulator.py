aac1f205abc319185b4c334053c736330689dc66
Teszáry Péter <peterteszary@gmail.com>
2022-11-10 20:20:49 +0100

    Házi feladat V2

--- Python Projects/szigeteles-kalkulator.py
+++ Python Projects/szigeteles-kalkulator.py
@@ -25,7 +25,44 @@ else :
     print("Kérem adja meg mire van szüksége: ")
     hungarocell = input("Hungarocell? i/n ")
     if hungarocell == "i":
-        homlokzatnegyzetmeter = int(input("Hány négyzetméter a homlokzat? "))
+        print("Hozzáadtam a hungarocellt a kalkulációhoz")
+    else :
+        print("Rendben.")
     ragaszto = input("Ragasztó (25kg)? i/n ")
+    if ragaszto == "i":
+        print("Hozzáadtam a ragasztót a kalkulációhoz")
+    else:
+        print("Rendben.")
+
     dubel = input("Dűbel (250db/csomag)? i/n ")
-    halo = input("Háló (250db/csomag)? i/n ")
+    if dubel == "i":
+        print("Hozzáadtam a dűbelt a kalkulációhoz")
+    else:
+        print("Rendben.")
+    halo = input("Háló (20nm/csomag)? i/n ")
+    if halo == "i":
+        print("Hozzáadtam a hálót a kalkulációhoz")
+    else:
+        print("Rendben.")
+
+print("A következő kalkuláció készült: ")
+
+hungarocell_ossz = (homlokzatnegyzetmeter + labazatnegyzetmeter) * hungarocell_ar
+ragaszto_ossz = (homlokzatnegyzetmeter + labazatnegyzetmeter) * ragaszto_ar
+dubel_ossz = (homlokzatnegyzetmeter + labazatnegyzetmeter) * dubel_ar
+halo_ossz = (homlokzatnegyzetmeter + labazatnegyzetmeter) * halo_ar
+
+mindossz = hungarocell_ossz + ragaszto_ossz + dubel_ossz + halo_ossz
+
+print("A hungarocell ára: " ,hungarocell_ossz, "Ft.")
+print("A ragasztó ára: " ,ragaszto_ossz, "Ft.")
+print("A dűbel ára: " ,dubel_ossz, "Ft.")
+print("A háló ára: " ,ragaszto_ossz, "Ft.")
+
+print("Mindösszesen: ", mindossz, "Ft.")
+
+rendeles = input("Megrendeli a kalkulációban szereplő tételeket? i/n: ")
+if rendeles == "i":
+    print ("A rendelését rögzítettük " '\U0001F603')
+else :
+    print("Köszönjük, hogy nálunk érdeklődött!")
