c292d0d8d96179514a2db1362f3b1b1a9d243f24
Teszáry Péter <peterteszary@gmail.com>
2023-01-16 20:05:05 +0100

    Sorting

---
+++ Python Projects/Practice3/diakok-jegyei.py
@@ -0,0 +1,3 @@
+# Definiálj egy függvényt, ami bekér egy diák jegyeit, és visszaadja a diák átlagjegyét.
+# Definiálj egy függvényt, ami bekér egy termék árát és áfakulcsát, és visszaadja a termék végleges árát.
+# Definiálj egy függvényt, ami bekér egy szöveget és egy betűt, és visszaadja hogy hányszor szerepel a szövegben a betű.
