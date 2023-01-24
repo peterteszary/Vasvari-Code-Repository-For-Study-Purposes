af139c9b46b3db5c3f789a45052f2f330ae2d74e
Teszáry Péter <peterteszary@gmail.com>
2023-01-24 21:11:26 +0100

    Java

--- Csharp Projects/HelloWorld.cs
+++ Csharp Projects/HelloWorld.cs
@@ -7,6 +7,7 @@ namespace ConsoleApplication {
         public static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Peter!");
         }
     }
 }
---
+++ JavaProjects/Hello.java
@@ -0,0 +1,6 @@
+class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("My first Java program");
+}
+}
