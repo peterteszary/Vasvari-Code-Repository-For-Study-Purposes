7859d379e3a3ec6a59f1c0e736837d7fe61341cd
Teszáry Péter <peterteszary@gmail.com>
2023-01-28 13:20:50 +0100

    javascript first commits.

---
+++ JavaScript Projects/elso.js
@@ -0,0 +1,16 @@
+let v = "kulso fajl";
+let v2 = 45;
+consolelog(v, typeof v2, v2); //Tesztőleges paramétere lehet vesszővel elválasztva.
+let g;
+console.log(typeof g); // undefined
+const b = true; // Konstansnak egyszer adhatok értéket!
+console.log(!b, typeof b);
+const str = "Szöveg";
+let o = {
+    name: "Géza",
+    age: "20",
+};
+console.table(o);
+console.log(o.name);
+// let v = "kulso fajl"
+// console.log(v)
