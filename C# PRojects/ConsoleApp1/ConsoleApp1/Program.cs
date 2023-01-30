e621fa32ba67998f6daf0eb633a61b898ce37d0c
Teszáry Péter <peterteszary@gmail.com>
2023-01-30 16:46:16 +0100

    File reading exercise

---
+++ C# PRojects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] items = new string[] { "zokni", "nadrág", "póló", "ing", "sapka" };
+        Random r = new Random();
+        try
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\bsze2tespet\Documents\GitHub\Vasvari-Code-Repository-For-Study-Purposes\C# PRojects\filewrite\app\Program.cs", true, Encoding.utf - 8);
+            for (int i = 0; i < 500; i++)
+            {
+                sw.WriteLine(items[r.Next(0, 4)]);
+            }
+            sw.Flush();
+            sw.Close();
+        }
+
+        catch (DirectoryNotFoundException) { Console.WriteLine("A könyvtár nem létezik!"); }
+        catch (IOException) { Console.WriteLine("Ismeretlen hiba!"); }
+
+    }
+
+}
