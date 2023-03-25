6dc1585ff15e6d86c9a30f3715e52935c87b46a4
Teszáry Péter <peterteszary@gmail.com>
2023-03-25 11:57:35 +0100

    push

---
+++ C# PRojects/SzovegFeldolgozo/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SzovegFeldolgozo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.szovegTerulet = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fajlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.megnyitasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.szavak = new System.Windows.Forms.ListBox();
+            this.feldolgozasGomb = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // szovegTerulet
+            // 
+            this.szovegTerulet.Location = new System.Drawing.Point(45, 46);
+            this.szovegTerulet.Name = "szovegTerulet";
+            this.szovegTerulet.Size = new System.Drawing.Size(708, 124);
+            this.szovegTerulet.TabIndex = 0;
+            this.szovegTerulet.Text = "";
+            this.szovegTerulet.TextChanged += new System.EventHandler(this.szovegTerulet_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fajlToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fajlToolStripMenuItem
+            // 
+            this.fajlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.megnyitasToolStripMenuItem,
+            this.mentesToolStripMenuItem});
+            this.fajlToolStripMenuItem.Name = "fajlToolStripMenuItem";
+            this.fajlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fajlToolStripMenuItem.Text = "Fájl";
+            // 
+            // megnyitasToolStripMenuItem
+            // 
+            this.megnyitasToolStripMenuItem.Name = "megnyitasToolStripMenuItem";
+            this.megnyitasToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.megnyitasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.megnyitasToolStripMenuItem.Text = "Megnyitás";
+            this.megnyitasToolStripMenuItem.Click += new System.EventHandler(this.megnyitasToolStripMenuItem_Click_1);
+            // 
+            // mentesToolStripMenuItem
+            // 
+            this.mentesToolStripMenuItem.Name = "mentesToolStripMenuItem";
+            this.mentesToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mentesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mentesToolStripMenuItem.Text = "Mentés";
+            this.mentesToolStripMenuItem.Click += new System.EventHandler(this.mentesToolStripMenuItem_Click_1);
+            // 
+            // szavak
+            // 
+            this.szavak.FormattingEnabled = true;
+            this.szavak.Location = new System.Drawing.Point(45, 242);
+            this.szavak.Name = "szavak";
+            this.szavak.Size = new System.Drawing.Size(708, 160);
+            this.szavak.TabIndex = 2;
+            // 
+            // feldolgozasGomb
+            // 
+            this.feldolgozasGomb.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.feldolgozasGomb.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.feldolgozasGomb.Location = new System.Drawing.Point(333, 188);
+            this.feldolgozasGomb.Name = "feldolgozasGomb";
+            this.feldolgozasGomb.Size = new System.Drawing.Size(142, 35);
+            this.feldolgozasGomb.TabIndex = 3;
+            this.feldolgozasGomb.Text = "Feldolgozás";
+            this.feldolgozasGomb.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.feldolgozasGomb);
+            this.Controls.Add(this.szavak);
+            this.Controls.Add(this.szovegTerulet);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Főablak";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox szovegTerulet;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fajlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem megnyitasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mentesToolStripMenuItem;
+        private System.Windows.Forms.ListBox szavak;
+        private System.Windows.Forms.Button feldolgozasGomb;
+    }
+}
+
