41f981c07383a722c75b8c1d4adf13af69ed052c
Teszáry Péter <peterteszary@gmail.com>
2023-04-11 19:56:56 +0200

    Házik C#

---
+++ C# PRojects/ListakApp/ListakApp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListakApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Az első listához tartozó gombok
+
+        private void ElsoHozzaAdGomb_Click(object sender, EventArgs e)
+        {
+            // Ellenőrzés, hogy a szövegbeviteli mező nem üres
+            if (!string.IsNullOrEmpty(ElsoBevitelBox.Text))
+            {
+                // A szöveg hozzáadása az első listához
+                ElsoLista.Items.Add(ElsoBevitelBox.Text);
+                ElsoBevitelBox.Clear(); // A szövegbeviteli mező törlése
+            }
+        }
+
+        private void ElsoFelGomb_Click(object sender, EventArgs e)
+        {
+            int index = ElsoLista.SelectedIndex;
+            if (index > 0)
+            {
+                string selected = ElsoLista.SelectedItem.ToString();
+                ElsoLista.Items.RemoveAt(index);
+                ElsoLista.Items.Insert(index - 1, selected);
+                ElsoLista.SetSelected(index - 1, true);
+            }
+        }
+
+        private void ElsoLeGomb_Click(object sender, EventArgs e)
+        {
+            int index = ElsoLista.SelectedIndex;
+            if (index >= 0 && index < ElsoLista.Items.Count - 1)
+            {
+                string selected = ElsoLista.SelectedItem.ToString();
+                ElsoLista.Items.RemoveAt(index);
+                ElsoLista.Items.Insert(index + 1, selected);
+                ElsoLista.SetSelected(index + 1, true);
+            }
+        }
+
+        private void ElsoAlulraGomb_Click(object sender, EventArgs e)
+        {
+            int index = ElsoLista.SelectedIndex;
+            if (index >= 0 && index < ElsoLista.Items.Count - 1)
+            {
+                string selected = ElsoLista.SelectedItem.ToString();
+                ElsoLista.Items.RemoveAt(index);
+                ElsoLista.Items.Add(selected);
+                ElsoLista.SetSelected(ElsoLista.Items.Count - 1, true);
+            }
+        }
+
+        private void ElsoTorolGomb_Click(object sender, EventArgs e)
+        {
+            int index = ElsoLista.SelectedIndex;
+            if (index >= 0)
+            {
+                ElsoLista.Items.RemoveAt(index);
+            }
+        }
+
+        // A második listához tartozó gombok
+
+        private void MasodikHozzaAdGomb_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(MasodikBevitelBox.Text))
+            {
+                MasodikLista.Items.Add(MasodikBevitelBox.Text);
+                MasodikBevitelBox.Clear();
+            }
+        }
+
+        private void MasodikFelulreGomb_Click(object sender, EventArgs e)
+        {
+            if (MasodikLista.SelectedItem != null)
+            {
+                int index = MasodikLista.SelectedIndex;
+                MasodikLista.Items.Insert(0, MasodikLista.SelectedItem);
+                MasodikLista.Items.RemoveAt(index + 1);
+                MasodikLista.SetSelected(0, true);
+            }
+        }
+
+        private void MasodikFelGomb_Click(object sender, EventArgs e)
+        {
+            if (MasodikLista.SelectedItem != null && MasodikLista.SelectedIndex > 0)
+            {
+                int index = MasodikLista.SelectedIndex;
+                MasodikLista.Items.Insert(index - 1, MasodikLista.SelectedItem);
+                MasodikLista.Items.RemoveAt(index + 1);
+                MasodikLista.SetSelected(index - 1, true);
+            }
+        }
+
+        private void MasodikLeGomb_Click(object sender, EventArgs e)
+        {
+            int index = MasodikLista.SelectedIndex;
+            if (MasodikLista.SelectedItem != null && index < MasodikLista.Items.Count - 1)
+            {
+                MasodikLista.Items.Insert(index + 2, MasodikLista.SelectedItem);
+                MasodikLista.Items.RemoveAt(index);
+                MasodikLista.SetSelected(index + 1, true);
+            }
+        }
+
+        private void MasodikAlulraGomb_Click(object sender, EventArgs e)
+        {
+            if (MasodikLista.SelectedItem != null)
+            {
+                int index = MasodikLista.SelectedIndex;
+                MasodikLista.Items.Insert(MasodikLista.Items.Count, MasodikLista.SelectedItem);
+                MasodikLista.Items.RemoveAt(index);
+                MasodikLista.SetSelected(MasodikLista.Items.Count - 1, true);
+            }
+        }
+
+        private void MasodikTorolGomb_Click(object sender, EventArgs e)
+        {
+            if (MasodikLista.SelectedItem != null)
+            {
+                MasodikLista.Items.Remove(MasodikLista.SelectedItem);
+            }
+        }
+
+        private void BalraGomb_Click(object sender, EventArgs e)
+        {
+            if (MasodikLista.SelectedItem != null)
+            {
+                ElsoLista.Items.Add(MasodikLista.SelectedItem);
+                MasodikLista.Items.Remove(MasodikLista.SelectedItem);
+            }
+        }
+
+        private void JobbraGomb_Click(object sender, EventArgs e)
+        {
+            if (ElsoLista.SelectedItem != null)
+            {
+                MasodikLista.Items.Add(ElsoLista.SelectedItem);
+                ElsoLista.Items.Remove(ElsoLista.SelectedItem);
+            }
+        }
+    }
+}
---
+++ C# PRojects/SzamGeneraloLista/SzamGeneraloLista/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SzamGeneraloLista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void GeneraloGomb_Click(object sender, EventArgs e)
+        {
+            // töröljük a korábban generált számokat a listából
+            GeneraltSzamok.Items.Clear();
+
+            // generálunk 1000 random számot és hozzáadjuk a listához
+            Random rand = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                int randomSzam = rand.Next(1, 10001);
+                GeneraltSzamok.Items.Add(randomSzam);
+            }
+        }
+
+        private void GeneraltSzamok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GeneralasOpciok_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrimRadioGomb_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ParosRadioGomb_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ParatlanRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HarommalRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HattalRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KilenccelRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NegyzetRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MutatButton_Click(object sender, EventArgs e)
+        {
+          
+        }   
+    private void EredmenyLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HozzaadottLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
