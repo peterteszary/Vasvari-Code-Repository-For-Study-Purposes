3470bc25a734201af811cd6232165ebec66137cd
Teszáry Péter <peterteszary@gmail.com>
2023-05-16 20:24:25 +0200

    update 2023-05-16

--- WebDev Projects/mobiltelefonok_peterteszary/js/sections.js
+++ WebDev Projects/mobiltelefonok_peterteszary/js/sections.js
@@ -1,24 +1,44 @@
+
 function loadSections() {
-fetch('../sections/cards.html')
-  .then(response => response.text())
-  .then(html => {
-    const div = document.querySelector('#cards');
-    div.innerHTML = html;
-  });
 
 
-fetch('../sections/hero.html')
-.then(response => response.text())
-.then(html => {
-  const div = document.querySelector('#hero');
-  div.innerHTML = html;
-});
+  fetch('../sections/hero.html')
+    .then(response => response.text())
+    .then(html => {
+      const div = document.querySelector('#hero');
+      div.innerHTML = html;
+    });
 
 
-fetch('../sections/three-columns.html')
-.then(response => response.text())
-.then(html => {
-  const div = document.querySelector('#three-columns');
-  div.innerHTML = html;
-});
+  fetch('../sections/three-columns.html')
+    .then(response => response.text())
+    .then(html => {
+      const div = document.querySelector('#three-columns');
+      div.innerHTML = html;
+    });
+
+
+  fetch('../sections/cards.html')
+    .then(response => response.text())
+    .then(html => {
+      const div = document.querySelector('#cards');
+      div.innerHTML = html;
+    });
+
+
+  fetch('../sections/cards-second.html')
+    .then(response => response.text())
+    .then(html => {
+      const div = document.querySelector('#cards-second');
+      div.innerHTML = html;
+    });
+
+  fetch('../sections/cards-third.html')
+    .then(response => response.text())
+    .then(html => {
+      const div = document.querySelector('#cards-third');
+      div.innerHTML = html;
+    });
+
+
 }
