a9ee6892e1612f4751c4232362ca3d7b4794a7f3
Teszáry Péter <peterteszary@gmail.com>
2023-05-18 14:52:20 +0200

    Middle of the project

--- WebDev Projects/mobiltelefonok_peterteszary/js/sections.js
+++ WebDev Projects/mobiltelefonok_peterteszary/js/sections.js
@@ -40,5 +40,12 @@ function loadSections() {
       div.innerHTML = html;
     });
 
+    fetch('../sections/devices-section.html')
+    .then(response => response.text())
+    .then(html => {
+      const div = document.querySelector('#devices');
+      div.innerHTML = html;
+    });
+
 
 }
