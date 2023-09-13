709683a5058974fd3fed3891da0fb4cb55a2d3fd
Teszáry Péter <peterteszary@gmail.com>
2023-09-13 17:23:55 +0200

    Push to update

---
+++ C# PRojects/gyakorlas-2023-06-05/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace SzamElemzo
+{
+    public partial class MainWindow : Window
+    {
+        private List<int> szamok;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SzamokGenerálása_Click(object sender, RoutedEventArgs e)
+        {
+            szamok = VeletlenSzamokGeneralasa();
+            SzamokSzamlalasaFrissitese();
+            SzamokListBoxFrissitese();
+        }
+
+        private List<int> VeletlenSzamokGeneralasa()
+        {
+            Random random = new Random();
+            return Enumerable.Range(1, 100)
+                .Select(_ => random.Next(1, 1000))
+                .ToList();
+        }
+
+        private void SzamokSzamlalasaFrissitese()
+        {
+            int parosSzamokSzama = szamok.Count(n => n % 2 == 0);
+            int paratlanSzamokSzama = szamok.Count(n => n % 2 != 0);
+            int primSzamokSzama = szamok.Count(IsPrim);
+
+            parosSzamokTextBlock.Text = parosSzamokSzama.ToString();
+            paratlanSzamokTextBlock.Text = paratlanSzamokSzama.ToString();
+            primSzamokTextBlock.Text = primSzamokSzama.ToString();
+        }
+
+        private bool IsPrim(int szam)
+        {
+            if (szam < 2)
+                return false;
+
+            for (int i = 2; i * i <= szam; i++)
+            {
+                if (szam % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void SzamokListBoxFrissitese()
+        {
+            szamokListBox.ItemsSource = szamok;
+        }
+
+        private void SzamokMentése_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Szöveges fájlok|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllLines(saveFileDialog.FileName, szamok.Select(n => n.ToString()));
+                MessageBox.Show("Számok sikeresen mentve.");
+            }
+        }
+    }
+}
---
+++ C# PRojects/utolso_projekt/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace utolso_projekt
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton elsoRadioButton = GetSelectedRadioButton("elsoszakasz");
+            RadioButton masodikRadioButton = GetSelectedRadioButton("masodikszakasz");
+
+            if (elsoRadioButton != null && masodikRadioButton != null)
+            {
+                Color szin1 = GetSzinByRadioButton(elsoRadioButton);
+                Color szin2 = GetSzinByRadioButton(masodikRadioButton);
+
+                LinearGradientBrush gradiensKeverek = new LinearGradientBrush(szin1, szin2, 0);
+                wrapPanel.Background = gradiensKeverek;
+            }
+        }
+
+        private Color GetSzinByRadioButton(RadioButton radioButton)
+        {
+            switch (radioButton.Content.ToString())
+            {
+                case "Első":
+                    return Colors.Red;
+                case "Második":
+                    return Colors.Green;
+                case "Harmadik":
+                    return Colors.Blue;
+                case "Negyedik":
+                    return Colors.Yellow;
+                case "Ötödik":
+                    return Colors.Orange;
+                case "Hatodik":
+                    return Colors.Purple;
+                default:
+                    return Colors.Black;
+            }
+        }
+
+        private RadioButton GetSelectedRadioButton(string groupName)
+        {
+            foreach (var radioButton in FindVisualChildren<RadioButton>(this))
+            {
+                if (radioButton.GroupName == groupName && radioButton.IsChecked == true)
+                    return radioButton;
+            }
+            return null;
+        }
+
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject dependencyObject) where T : DependencyObject
+        {
+            if (dependencyObject != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(dependencyObject, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+    }
+}
