5a327555c7c3642516c94fdbb1b87cd4dab2e831
Teszáry Péter <peterteszary@gmail.com>
2023-09-21 17:53:50 +0200

    Button added

--- FrontEnd/jquery/script.js
+++ FrontEnd/jquery/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     //Teljes oldal letöltésre került..
     $(".kek").click(function(){
-        $(this).hide(1000)
+        $(this).hide(2000);
     })
-})
+    $('.btn-kek').click(function(){
+       // $(".kek").show();
+         $(".kek").toggle(1000);
+    })
+    })
