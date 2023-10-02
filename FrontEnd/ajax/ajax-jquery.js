898639da7868c0c5f47e2494c3615b7734b45ea5
Teszáry Péter <peterteszary@gmail.com>
2023-10-02 16:32:45 +0200

    bugfix

--- FrontEnd/ajax/ajax-jquery.js
+++ FrontEnd/ajax/ajax-jquery.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
     $.get('https://jsonplaceholder.typicode.com/users/1', function(d){
         console.log(Object.keys(d)) //Objektum kulcsainak lekérdezése egy tömbbe.
-        console.log(Object.values(d)) // Objektum kulcsokhoz tartozó értékek lekérdezése
+       // console.log(Object.values(d)) // Objektum kulcsokhoz tartozó értékek lekérdezése
+
+        //A Kapot objektumot teljes mértékben járjuk be és jelenítsük meg egy egymásba ágyazott listában.
+
+        if (typeof d == 'object'){
+            $(".content").append("<ul></ul>");
+            for (const key in d) {
+                if (Object.hasOwnProperty.call(d, key)) {
+                   $("ul").append(`<li>${key} : ${d[key]}</li>`) 
+
+                    
+                    
+                }
+            }
+        }
     })
 })
