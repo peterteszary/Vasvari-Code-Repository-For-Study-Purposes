a06b83fc02b03c46e9b2f848c4549e920aeafc45
Teszáry Péter <peterteszary@gmail.com>
2023-11-05 14:26:42 +0100

    HomeWork

---
+++ FrontEnd/vue-teszary/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <h1>Hello World!</h1>
+  </template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({ count: 0 })
+const message = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">count is: {{ count }}</button>
+</template>
