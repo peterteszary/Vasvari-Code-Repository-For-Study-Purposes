af66c743b01f24cb7fe2d2487cba99dca2d03a61
peterteszary <peterteszary@gmail.com>
2024-01-24 16:17:28 +0100

    initial commit json server axios

---
+++ FrontEnd/esti/json-server-axios-demo/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+const posts = ref();
+
+
+onMounted(() => {
+  fetch('http://localhost:3000/posts')
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json);
+    posts.value = json;
+  })
+})
+</script>
+
+<template>
+ 
+</template>
