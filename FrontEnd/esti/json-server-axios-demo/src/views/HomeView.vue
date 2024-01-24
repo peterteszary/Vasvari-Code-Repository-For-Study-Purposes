cf0b3304f76de328ac74fcef7ad10cd63ab058d4
peterteszary <peterteszary@gmail.com>
2024-01-24 17:12:39 +0100

    shorted code

--- FrontEnd/esti/json-server-axios-demo/src/views/HomeView.vue
+++ FrontEnd/esti/json-server-axios-demo/src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <script setup>
 import { onMounted, ref } from 'vue';
-const posts = ref();
+import axios from 'axios';
 
+const posts = ref([]);
+const comments = ref([]); 
 
 onMounted(() => {
   fetch('http://localhost:3000/posts')
-  .then(resp => resp.json())
-  .then(json => {
-    console.log(json);
-    posts.value = json;
-  })
-})
+    .then(resp => resp.json())
+    .then(json => {
+      console.log(json);
+      posts.value = json;
+    });
+});
+
+onMounted(() => {  
+  axios.get('http://localhost:3000/comments')
+    .then(resp => {
+      console.log(resp.data);
+      comments.value = resp.data;
+
+    })
+     
+     
+    })
+
 </script>
 
 <template>
- 
+  <div>
+    <table v-if="posts.length > 0">
+      <tr>
+        <th>Id</th>
+        <th>Title</th>
+      </tr>
+      <tr v-for="p in posts" :key="p.id">
+        <td>{{ p.id }}</td>
+        <td>{{ p.title }}</td>
+      </tr>
+    </table>
+    <p v-else>Nincs adat!</p>
+    <hr>
+    <table v-if="comments.length > 0">
+      <tr>
+        <th>Id</th>
+        <th>Comment</th> <!-- Adjust the property name here -->
+      </tr>
+      <tr v-for="c in comments" :key="c.id">
+        <td>{{ c.id }}</td>
+        <td>{{ c.text }}</td> <!-- Adjust the property name here -->
+      </tr>
+    </table>
+    <p v-else>Nincs adat!</p>
+  </div>
 </template>
