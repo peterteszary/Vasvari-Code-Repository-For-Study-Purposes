5179b17e5a6c156be824dcc7d9069e39642f425b
peterteszary <peterteszary@gmail.com>
2024-02-12 18:43:16 +0100

    update

--- FrontEnd/todo-axios-json/todo-axios-json/src/views/NewTaskView.vue
+++ FrontEnd/todo-axios-json/todo-axios-json/src/views/NewTaskView.vue
@@ -1,11 +1,40 @@
-<script>
-  
-</script>
 
 <template>
+    <form class="w-75 mx-auto" @submit.prevent ="saveTask()">
+        <input v-model="task" class="form-control mt-3" type="text" placeholder="Új feladat">
+        <input v-model="date" class="form-control mt-3" type="date">
+        <input class="btn btn-success w-100 mt-3" type="submit" value="Rögzít">
 
+    </form>
+    <h3 class="text-danger text-center pt-5" v-if="error">Minden mezőt tölts ki!</h3>
 </template>
 
-<style>
+<script setup>
+import axios from 'axios';
+import {ref} from 'vue';
+const task = ref("");
+const date = ref("");
+const error = ref(false);
 
-</style>
+const saveTask = () => {
+    console.log(task.value, date.value);
+    if (task.value.trim() < 3 || date.value.trim().length != 10 ) {
+        error.value = true;
+    }
+    else { //Az adatok küldése is itt történik.
+        error.value = false;
+        let shadowCopy = [];
+        axios.get("http://localhost:3000/todos")
+        .then(resp => {
+            shadowCopy.push(resp.data);
+        });
+        console.log(Array .isArray(shadowCopy)); 
+
+        let nextId = shadowCopy[shadowCopy.length - 1] .id + 1;
+        console.log(nextId);
+        
+        //axios.post("http://localhost:3000/todos", data) 
+          
+    }
+}
+</script>
